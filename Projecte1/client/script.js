55c324dd36fd47bc23243c2301a91965b84695a3
Deriva176 <adaw25dflores@inslaferreria.cat>
2026-09-23 19:23:19 +0200

    f

--- Projecte1/client/script.js
+++ Projecte1/client/script.js
@@ -5,4 +5,14 @@ function saluda() {
 const boto = document.getElementById("btnSaluda");
 boto.addEventListener("click", saluda);
 
+// frontend/script.js — la manipulació d'aquesta setmana
+const titol = document.querySelector("#titolPrincipal");
+titol.textContent = "📮 El Cartero Invisible – Setmana 2";
+titol.setAttribute("data-role", "banner");
+
+const contenidor = document.querySelector("#contenidorCartes");
+contenidor.innerHTML += "<p>Cartes pendents: 0</p>";
+
+const info = document.querySelector(".info");
+info.style.color = "#2c3e50";
 
